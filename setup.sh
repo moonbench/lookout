ce41a68141f71ea90c66fe45bbbfa9e8195254b8
watchtower <watchtower@moonbench>
2018-02-18 11:23:03 +0000

    Add scripts

--- setup.sh
+++ setup.sh
@@ -4,38 +4,105 @@ HOSTNAME="watchtower1"
 
 declare ROOT_DIR="$(dirname $(readlink -f $0))"
 
+HIGHLIGHT_COLOR='\e[36m'
+TITLE_COLOR='\e[4m\e[1m'
+DONE_COLOR='\e[1;32m'
+NO_COLOR='\e[0m'
+
+debug(){
+  echo -e "${HIGHLIGHT_COLOR}${1}${NO_COLOR}"
+}
+title(){
+  echo -e "${TITLE_COLOR}${1}${NO_COLOR}"
+}
+success(){
+  echo -e "${DONE_COLOR}${TITLE_COLOR}${1}${NO_COLOR}"
+}
+say_done(){
+  local donemsg="Done."
+  if [[ ! -z "${1}" ]]; then
+    donemsg=$1
+  fi
+  echo -e "${DONE_COLOR}$donemsg${NO_COLOR}"
+}
+
 echo "Setting up the watchtower..."
 
-echo "Setting up the home directory for the watchtower's shared files..."
-mkdir /var/watchtower
-mkdir /var/watchtower/cameras
-mkdir /var/log/watchtower
-echo -e "Done.\\n"
+setup_directories(){
+  debug "Setting up project directories..."
+  mkdir /var/log/watchtower
+  mkdir /var/watchtower
+  mkdir /var/watchtower/cameras
+  mkdir /var/watchtower/config
+  copy_scripts
+  say_done "Directories ready."
+}
+copy_scripts(){
+  debug "Copying in scripts..."
+  cp -r ${ROOT_DIR}/scripts /var/watchtower/scripts
+  say_done "Scripts copied."
+}
 
-echo "Setting up the users for the system..."
-userdel -f pi
-useradd -m camera
-useradd -m reader
-echo -e "Done.\\n"
+setup_users(){
+  debug "Setting up users..."
+  remove_pi_user
+  useradd -m camera
+  useradd -m reader
+  say_done "Users ready."
+}
+remove_pi_user(){
+  debug "Removing pi user..."
+  userdel -f pi
+  say_done
+}
 
-echo "Setting up networking..."
+setup_network(){
+  debug "Setting up networking..."
+  set_hostname
+  enable_ssh
+  say_done "Network configured."
+}
+set_hostname(){
+  debug "Setting hostname..."
+  echo "${HOSTNAME}" > /etc/hostname
+  hostname "${HOSTNAME}"
+  say_done
+}
+enable_ssh(){
+  debug "Enabling ssh..."
+  systemctl enable ssh
+  systemctl start ssh
+  say_done
 
-echo "Setting hostname..."
-echo "${HOSTNAME}" > /etc/hostname
-hostname "${HOSTNAME}"
-echo "Done."
+  debug "Adding group for ssh users..."
+  groupadd -r sshusers
+  usermod -a -G sshusers reader
+  echo "AllowGroups sshusers" >> /etc/ssh/sshd_config
+  /etc/init.d/ssh restart
+  say_done
+}
 
-echo "Enabling ssh..."
-systemctl enable ssh
-systemctl start ssh
-echo "Adding ssh group..."
-groupadd -r sshusers
-usermod -a -G sshusers reader
-echo "AllowGroups sshusers" >> /etc/ssh/sshd_config
-/etc/init.d/ssh restart
-echo "Done."
-echo "Done."
+install_software(){
+  update_aptget
+  install_libav
+}
+update_aptget(){
+  debug "Updating apt-get.."
+  apt-get update
+  apt-get upgrade -y
+  apt-get autoremove
+  apt-get autoclean
+  debug "Updated."
+}
+install_libav(){
+  apt-get install -y libav-tools
+}
 
 
-echo -e "Done.\\n"
+title "New tower setup"
+setup_directories
+setup_users
+setup_network
+install_software
+success "Tower ready!"
 
